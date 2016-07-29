b7772f50046beb2ca9e2d8f86ef7bae21c0cd3b7
davidallenmann <dm@humu.io>
2016-07-28 22:54:33 -0400

    read OpenTag file into R

---
+++ R/otload.R
@@ -0,0 +1,78 @@
+
+filename = "/w/loggerhead/R/opentag/M1.DSG"
+
+datafile = file(filename, "rb")
+
+# DF_HEAD
+version = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+userID = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+second = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+minute = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+hour = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+day = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+mday = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+month = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+year = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+timezone = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+
+if (version >= 1010){
+  lat = readBin(datafile, numeric(), n = 1, size = 4, endian = "little")
+  lon = readBin(datafile, numeric(), n = 1, size = 4, endian = "little")
+  depth = readBin(datafile, numeric(), n = 1, size = 4, endian = "little")
+  DSGcal = readBin(datafile, numeric(), n = 1, size = 4, endian = "little")
+  hydroCal = readBin(datafile, numeric(), n = 1, size = 4, endian = "little")
+  lpFilt = readBin(datafile, numeric(), n = 1, size = 4, endian = "little")
+}
+
+# SID_SPEC
+notdone = 1
+nSIDSPEC = 1
+SID <- vector()
+while (notdone == 1){
+  SID[nSIDSPEC] = intToUtf8(readBin(datafile, integer(), n = 4, size = 1, endian = "little"))
+  nBytes[nSIDSPEC] = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+  numChan[nSIDSPEC] = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+  storeType[nSIDSPEC] = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+  sensorType[nSIDSPEC] = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+  dForm[nSIDSPEC] = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+  period[nSIDSPEC] = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+  recpts[nSIDSPEC] = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+  recint[nSIDSPEC] = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+  
+  if(nBytes[nSIDSPEC] == 0){
+    notdone = 0
+  }
+  nSIDSPEC = nSIDSPEC + 1
+}
+nSIDSPEC = nSIDSPEC - 2
+
+# SID_REC 
+
+# IMU dataframe
+# Pressure/Temperature dataframe
+while (1) {
+    nSID = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+    chan = readBin(datafile, integer(), n = 1, size = 1, endian = "little")
+    nbytes = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+    if (version > 9999){
+      nbytes_2 = readBin(datafile, integer(), n = 1, size = 4, endian = "little")
+    }
+    cur_sid = nSID + 1
+    if(cur_sid>0 & cur_sid<8){
+      if(dForm[cur_sid] == 2){
+        nsamples = nBytes[cur_sid] / 2
+        chunk = readBin(datafile, integer(), n = nsamples, size = 2, endian = "little")
+      }
+      if(dForm[cur_sid] == 3){
+        nsamples = nBytes[cur_sid] / 3
+        chunk = readBin(datafile, integer(), n = nsamples, size = 3, endian = "little")
+      }
+      if(dForm[cur_sid] == 4){
+        nsamples = nBytes[cur_sid] / 4
+        chunk = readBin(datafile, numeric(), n = nsamples, size = 4, endian = "little")
+      }
+      # add to appropriate dataframe as read in
+      if (length(chunk) == nsamples) break;
+    }
+}
+close(datafile)
