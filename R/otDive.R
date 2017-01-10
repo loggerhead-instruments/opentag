f4e2be115ed23688c6b644f5d5e85709762f0103
davidallenmann <dm@humu.io>
2017-01-09 19:47:36 -0500

    update

--- R/otDive.R
+++ R/otDive.R
@@ -1,15 +1,16 @@
 # otDive
+# OpenTag files should be loaded with otload or from file
 
 require(signal)
 require(stats)
 require(lubridate)
-# files should be loaded with otload or from file
-
 
 # Calculate depth
 surfacePressure = 1013.25 # mbar
 barPerMeter = 0.100693064 # bar at 1 m
 PTMP$depth = (PTMP$pressure - surfacePressure) / (1000.0 * barPerMeter)
+
+plot(PTMP$datetime, PTMP$depth, type='l')
 
 # Median filter depth to remove bad values
 medFiltDur = 2 # median filter duration in seconds
@@ -21,7 +22,6 @@ PTMP$depth = min(PTMP$depth) - PTMP$depth
 
 plot(PTMP$datetime, PTMP$depth, type='l')
 
-
 # Calculate pitch, roll, yaw
 radPerDeg = 0.0174532925
 # roll
@@ -32,7 +32,6 @@ cosAngle = cos(phi)
 # de-rotate by roll angle
 Bfy = (INER$magY * cosAngle) - (INER$magZ * sinAngle)
 Bz = (INER$magY * sinAngle) + (INER$magZ * cosAngle)
-
 Gz = INER$accelY * sinAngle + INER$accelZ * cosAngle
 
 # theta = pitch angle (-90 to 90 degrees)
@@ -60,7 +59,38 @@ INER$VDBA = sqrt((INER$accelX - stats::filter(INER$accelX, rep(1/n, n), sides = 
                 (INER$accelZ - stats::filter(INER$accelZ, rep(1/n, n), sides = 2))^2)
 
 # Dive detection
+diveDepth = -1
+dives = PTMP$depth < diveDepth
+diffDives = diff(dives)
+
+# # number dives and surface intervals incrementally
+# diveNum = 1
+# surfaceNum = 1
+# 
+# index = 0
+# lastDiveTime = dive.index[0]
+# firstDive = 1
+# diveLabel = ''
+# curDiveTime = ''
+# timediff = dive.index[index] - dive.index[index]
+# 
+# # vectorized version
+# 
+# surface = dive['DepthDiff'][(dive['DepthDiff']==-1)] 
+# divedive = dive['DepthDiff'][(dive['DepthDiff']==1)]
+# 
+# dive['DiveStartTime'] = pd.DataFrame({'DiveTime': dive.index[(dive['DepthDiff']==1) | (dive['DepthDiff']==-1)]}, index=dive.index[(dive['DepthDiff']==1) | (dive['DepthDiff']==-1)])
+# # increment values
+# ss = surface * (np.arange(surface.size)+1)
+# dd = divedive  * (np.arange(divedive.size)+1)
+# 
+# dive['dive'] = pd.concat([ss,dd])
+# 
+# dive.fillna(method='ffill', inplace=True)
+
+# Give each dive a dive number
 
 
-# Metrics by dive
+# Metrics by dive 
+# look at aggregate function
 
